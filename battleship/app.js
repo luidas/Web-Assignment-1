20b31a4bc067e689cb6d721293b03ac3905c39de
Liudas Mikalauskas <liudasmika@gmail.com>
2018-12-07 18:39:31 +0100

    labas

--- battleship/app.js
+++ battleship/app.js
@@ -44,3 +44,18 @@ setInterval(function() {
 
 var currentGame = new Battleship(gameStatus.gamesInitialized++);
 var connectionID = 0;//each websocket receives a unique ID
+
+wss.on("connection", function connection(ws) {
+
+    /*
+     * two-player game: every two players are added to the same game
+     */
+    let con = ws; 
+    con.id = connectionID++;
+    let playerType = currentGame.addPlayer(con);
+    websockets[con.id] = currentGame;
+
+    console.log("Player %s placed in game %s as %s", con.id, currentGame.id, playerType);
+});
+
+server.listen(port);
