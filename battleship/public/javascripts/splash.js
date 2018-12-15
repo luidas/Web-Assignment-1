c266b192f2d78bda77dbe23040e58a297be489fe
Liudas Mikalauskas <liudasmika@gmail.com>
2018-12-15 18:46:33 +0100

    visit number

--- battleship/public/javascripts/splash.js
+++ battleship/public/javascripts/splash.js
@@ -1,4 +1,5 @@
 var playButton = document.getElementById("play");
+var visits = document.getElementById("visits");
 playButton.onclick = addLink;
 nickname = document.getElementById("nickname");
 
@@ -7,3 +8,8 @@ function addLink() {
     window.location.href = "/play?name=" + nickname.value;
     }
 }
+
+document.cookie+=".";
+var cookieValue = document.cookie.split("=")[1];
+visits.append(document.createTextNode("Visit number: " + document.cookie.length.toString()));
+//console.log(document.cookie);
